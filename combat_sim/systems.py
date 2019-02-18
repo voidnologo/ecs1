d437a3b0463c21c9a1557621f6d010192ed1e2a3
voidnologo <(none)>
2019-02-18 08:29:33 -0500

    remove breakpoints

--- combat_sim/systems.py
+++ combat_sim/systems.py
@@ -6,14 +6,12 @@ class CombatSystem(System):
     components = ['Health', 'Damage']
 
     def update(self, dt=None):
-        # breakpoint()
         cowboys = (e for e in self.entities if e.race.race == 'cowboy' and e.health.alive)
         aliens = (e for e in self.entities if e.race.race == 'alien' and e.health.alive)
 
         # Combat happens simultaneously and to the death
         cowboy = next(cowboys, None)
         alien = next(aliens, None)
-        # breakpoint()
 
         if cowboy and alien:
             while cowboy.health.alive and alien.health.alive:
